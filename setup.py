260939b9a3bf4517bf421726e4d758f42e2f62c8
Eitan Isaacson <eitan@ascender.com>
2008-08-26 15:45:39 -0700

    Added a customized py2exe build command. The speclenium script now attempts to start a Selenium server.

--- setup.py
+++ setup.py
@@ -3,7 +3,46 @@ A cross-platform accessibility API inspection library and service.
 """
 import sys, os
 from distutils.core import setup, Command, DistutilsOptionError
+import distutils.command
+import shutil
 import specular
+
+options = {}
+
+try:
+    import py2exe
+except ImportError:
+    # Not on windows or no py2exe, no biggie.
+    extras = {}
+else:
+    class bdist_win32_standalone(py2exe.build_exe.py2exe):
+        user_options = py2exe.build_exe.py2exe.user_options + \
+            [('selenium=', 'S', 'Selenium JAR file to include')]
+
+        def initialize_options(self):
+            self.selenium = 'selenium-server.jar'
+            py2exe.build_exe.py2exe.initialize_options(self)
+
+        def finalize_options(self):
+            if not os.path.exists(self.selenium):
+                raise DistutilsOptionError, \
+                    'Cannot find "%s". ' \
+                    'Use -S to point to a selenium server JAR file.' \
+                        % self.selenium
+            py2exe.build_exe.py2exe.finalize_options(self)
+
+        def run(self):
+            py2exe.build_exe.py2exe.run(self)
+            print 
+            print 'Copying', self.selenium, 'to', self.dist_dir
+            shutil.copy(self.selenium, 
+                        os.path.join(self.dist_dir, 'selenium-server.jar'))
+
+    extras = {'options' : {'bdist_win32_standalone' : 
+                           {'includes' : 'twisted.web.resource'}},
+              'console' : ['speclenium'],
+              'cmdclass' : {'bdist_win32_standalone' : bdist_win32_standalone}}
+
 
 classifiers = [
     'Development Status :: 3 - Alpha',
@@ -30,4 +69,4 @@ setup(name=__doc__.split('\n')[0],
       classifiers=classifiers,
       version=specular.__version__,
       packages=["specular", "specular.speclenium"],
-      scripts=["speclenium"])
+      scripts=["speclenium"], **extras)
