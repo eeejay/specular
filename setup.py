cf8b2494a605edb973f72b07a47c7d87725cba54
Eitan Isaacson <eitan@ascender.com>
2008-08-27 08:42:52 -0700

    Added ZIP archive option for the standalone dist.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ A cross-platform accessibility API inspection library and service.
 """
 import sys, os
 from distutils.core import setup, Command, DistutilsOptionError
+from distutils.archive_util import make_archive
 import distutils.command
 import shutil
 import specular
@@ -17,10 +18,12 @@ except ImportError:
 else:
     class bdist_win32_standalone(py2exe.build_exe.py2exe):
         user_options = py2exe.build_exe.py2exe.user_options + \
-            [('selenium=', 'S', 'Selenium JAR file to include')]
+            [('selenium=', 'S', 'Selenium JAR file to include'),
+             ('zip', 'Z', 'zip distribution')]
 
         def initialize_options(self):
             self.selenium = 'selenium-server.jar'
+            self.zip = False
             py2exe.build_exe.py2exe.initialize_options(self)
 
         def finalize_options(self):
@@ -30,13 +33,30 @@ else:
                     'Use -S to point to a selenium server JAR file.' \
                         % self.selenium
             py2exe.build_exe.py2exe.finalize_options(self)
+            if self.zip:
+                self.base_name = \
+                    self.distribution.get_fullname().lower()
+                self.archive_dir = os.path.join(self.dist_dir, self.base_name)
+                self.base_dir = self.dist_dir
+                self.dist_dir = self.archive_dir
+                os.makedirs(self.dist_dir)
 
         def run(self):
             py2exe.build_exe.py2exe.run(self)
+            print
+            print self.distribution.get_fullname().lower()
             print 
             print 'Copying', self.selenium, 'to', self.dist_dir
             shutil.copy(self.selenium, 
                         os.path.join(self.dist_dir, 'selenium-server.jar'))
+            if self.zip:
+#                print 'root_dir', os.path.dirname(self.dist_dir.rstrip(os.path.sep))
+                archive_name = make_archive(self.base_name + '.win32', 'zip', 
+                                             self.base_dir, self.base_name)
+                print
+                print 'Created', archive_name
+                shutil.rmtree(self.archive_dir)
+                print 'Deleted', self.archive_dir
 
     extras = {'options' : {'bdist_win32_standalone' : 
                            {'includes' : 'twisted.web.resource'}},
