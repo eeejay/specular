b85a5697de91f3eeaf897d625091e5055f903d8e
Eitan Isaacson <eitan@ascender.com>
2008-09-05 11:14:06 -0700

    Working standalone script.

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ A cross-platform accessibility API inspection library and service.
 import sys, os
 from distutils.core import setup, Command, DistutilsOptionError
 from distutils.archive_util import make_archive
-import distutils.command, distutils.command.sdist
+import distutils.command, distutils.command.build_py
 import shutil
 import specular, specular.speclenium
+from distutils.core import Command
 
 options = {}
 
@@ -17,22 +18,49 @@ class _include_selenium:
     def finalize_options(self):
         if self.distribution.data_files is None:
             self.distribution.data_files = []
-        print 'appending', self.selenium
-        self.distribution.dist_files.append(('', [self.selenium]))
+        self.distribution.data_files.append(('', [self.selenium]))
         
 
-class standalone(distutils.command.sdist.sdist, _include_selenium):
-    user_options = distutils.command.sdist.sdist.user_options + \
-        _include_selenium.user_options
+class standalone(distutils.command.build_py.build_py):
+    user_options = [('selenium=', 'S', 'Selenium JAR file to include')] + \
+        distutils.command.build_py.build_py.user_options
+    
     def initialize_options(self):
-        _include_selenium.initialize_options(self)
-        distutils.command.sdist.sdist.initialize_options(self)
-        print self.distribution.dist_files
+        distutils.command.build_py.build_py.initialize_options(self)
+        self.selenium = 'selenium-server.jar'
+        self.dist_dir = None
+
     def finalize_options(self):
-        distutils.command.sdist.sdist.finalize_options(self)
-    def read_manifest(self):
-        distutils.command.sdist.sdist.read_manifest(self)
-        self.filelist.append(self.selenium)
+        distutils.command.build_py.build_py.finalize_options(self)
+        if self.dist_dir is None:
+            self.dist_dir = "dist"
+        self.filelist = \
+            [(m[2], os.path.dirname(m[2])) for m in self.find_all_modules()]
+        self.filelist.append((self.selenium, ""))
+        self.filelist += [(s, "") for s in self.distribution.scripts]
+        for dest, data_files in self.distribution.data_files:
+            for f in data_files:
+                self.filelist.append((f, dest))
+
+    def run(self):
+        dest_root = \
+            os.path.join(self.dist_dir,self.distribution.get_fullname())
+        self.mkpath(dest_root)
+        for f, dest_dir in self.filelist:
+            dest_dir = os.path.join(dest_root, dest_dir)
+            dest_file = os.path.join(dest_root, f)
+            if not os.path.exists(dest_dir):
+                self.mkpath(dest_dir)
+            self.copy_file(f, dest_file)
+            if f in self.distribution.scripts:
+                print 'changing mode of', dest_file
+                os.chmod(dest_file, 493)
+
+        archive_name = \
+            self.make_archive(
+                os.path.join(self.dist_dir, self.distribution.get_fullname()), 
+                'gztar', self.dist_dir, self.distribution.get_fullname())
+        shutil.rmtree(dest_root)
 
 try:
     import py2exe
@@ -43,15 +71,18 @@ else:
     class standalone_win32(py2exe.build_exe.py2exe, _include_selenium):
         user_options = py2exe.build_exe.py2exe.user_options + \
             _include_selenium.user_options + \
-            [('zip', 'Z', 'zip distribution')]
+            [('zip', 'Z', 'zip distribution'),
+             ('selenium=', 'S', 'Selenium JAR file to include')]
 
         def initialize_options(self):
             self.zip = False
-            _include_selenium.initialize_options(self)
+            self.selenium = 'selenium-server.jar'
             py2exe.build_exe.py2exe.initialize_options(self)
 
         def finalize_options(self):
-            _include_selenium.finalize_options(self)
+            if self.distribution.data_files is None:
+                self.distribution.data_files = []
+            self.distribution.data_files.append(('', [self.selenium]))
             py2exe.build_exe.py2exe.finalize_options(self)
             if self.zip:
                 self.base_name = \
@@ -64,12 +95,10 @@ else:
         def run(self):
             py2exe.build_exe.py2exe.run(self)
             if self.zip:
-                archive_name = make_archive(self.base_name + '.win32', 'zip', 
-                                             self.base_dir, self.base_name)
-                print
-                print 'Created', archive_name
+                archive_name = make_archive(
+                    os.path.join(self.dist_dir, self.base_name + '.win32'),
+                    'zip', self.base_dir, self.base_name)
                 shutil.rmtree(self.archive_dir)
-                print 'Deleted', self.archive_dir
 
     extras = {'options' : {'standalone_win32' : 
                            {'includes' : 'twisted.web.resource'}},
