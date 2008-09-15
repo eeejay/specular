6c90cb9f02b6ac5d4e1a01f4cd73569cb794c787
Eitan Isaacson <eitan@sparky.(none)>
2008-09-15 12:50:25 -0700

    Make speclenium script more bomb-proof.

--- tests/settings.py
+++ tests/settings.py
@@ -2,14 +2,16 @@ configs = {
     'windows' : {
             'host' : '11.0.0.2',
             'browsers' : {
-                        'firefox3' : '*chrome'
-#                        'safari' : '*safari C:\Documents and Settings\Eitan\Desktop\webkit-nightly\Safari.exe'
+                        'firefox3' : '*chrome',
+                        'safari' : '*safari C:\Documents and Settings\Eitan\Desktop\webkit-nightly\Safari.exe'
                         }
             },
     'linux' : {
             'host' : 'localhost',
             'browsers' : {
-                        'firefox3' : '*chrome /usr/lib/firefox-3.0.1/firefox'
+                        'firefox3' : '*chrome /usr/lib/firefox-3.0.2/firefox',
+                        'midori' : '*custom /usr/bin/midori',
+                        'epiphany' : '*custom /usr/bin/epiphany'
                         }
             }
     }
